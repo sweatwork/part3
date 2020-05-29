2134a68002695234f57e85ace5424a4f20e488bd
Danish Parvez <22967284+sweatwork@users.noreply.github.com>
2020-05-29 11:44:21 +0530

    Add frontend production build

---
+++ build/precache-manifest.87c7860ec03fce941c7b5df9f9e33e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0460d36ecd99aea527aed92f7ec8435d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "866507191a411643b9f9",
+    "url": "/static/css/main.19006390.chunk.css"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866507191a411643b9f9",
+    "url": "/static/js/main.8b258ab5.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token("req-body", (req) => JSON.stringify(req.body));
 app.use(
